23dd9d6e1e302b00efa433a2bdc387874b42efa4
armyrunner9916 <steven.j.reitz@gmail.com>
2025-08-24 13:07:34 -0400

    Fix Vite configuration to resolve build errors

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@ import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { createLogger, defineConfig } from 'vite';
 
-const isDev = process.env.NODE_ENV !== 'production';
-let inlineEditPlugin, editModeDevPlugin;
-
-if (isDev) {
-	inlineEditPlugin = (await import('./plugins/visual-editor/vite-plugin-react-inline-editor.js')).default;
-	editModeDevPlugin = (await import('./plugins/visual-editor/vite-plugin-edit-mode.js')).default;
-}
-
 const configHorizonsViteErrorHandler = `
 const observer = new MutationObserver((mutations) => {
 	for (const mutation of mutations) {
@@ -194,7 +186,6 @@ export default defineConfig({
 	optimizeDeps: {
 	},
 	plugins: [
-		...(isDev ? [inlineEditPlugin(), editModeDevPlugin()] : []),
 		react(),
 		addTransformIndexHtml
 	],
